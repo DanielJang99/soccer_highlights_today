f159c78db997f72aecd34a12e1f759cb0ac0339b
DanielJang <daniel.jang.0621@gmail.com>
2021-03-10 03:11:26 +0900

    implemented nprogress for better  routing ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,20 @@ import "semantic-ui-css/semantic.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "../components/navbar";
 import type { AppProps } from "next/app";
+import NProgress from "nprogress";
+import { Router } from "next/router";
+import "nprogress/nprogress.css";
+
+NProgress.configure({ easing: "ease", speed: 300 });
+Router.events.on("routeChangeStart", () => {
+    NProgress.start();
+});
+Router.events.on("routeChangeComplete", () => {
+    NProgress.done();
+});
+Router.events.on("routeChangeError", () => {
+    NProgress.done();
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
