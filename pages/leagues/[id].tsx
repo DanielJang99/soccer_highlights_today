142607b8a3761b86720db025f6ee55064726cf09
DanielJang <daniel.jang.0621@gmail.com>
2021-03-12 23:48:10 +0900

    fixed league-page bug

--- pages/leagues/[id].tsx
+++ pages/leagues/[id].tsx
@@ -7,88 +7,75 @@ import Link from "next/link";
 import MatchProps from "../../api/interface";
 import Head from "next/head";
 import { MajorLeagues } from "../../FootballData";
+import { useRouter } from "next/router";
 
 type Params = {
     params: {
         id: string;
     };
 };
-interface LeagueGameProps {
-    LeagueGames: MatchProps[];
+interface GamesProps {
+    Games: MatchProps[];
 }
 
-export default function League({ LeagueGames }: LeagueGameProps) {
-    return (
-        <>
-            {LeagueGames && (
-                <>
-                    <Head>
-                        <title>
-                            {LeagueGames[0].competition.name.split(":")[1]}
-                        </title>
-                    </Head>
-                    <div style={{ width: "90%", margin: "auto" }}>
-                        <Header as="h2" style={{ paddingTop: "1.875rem" }}>
-                            {LeagueGames[0].competition.name.split(":")[1]}
-                        </Header>
-                        <Divider />
-                        <Grid columns={3}>
-                            <Grid.Row>
-                                {LeagueGames.map((game) => (
-                                    <Grid.Column>
-                                        <Link
-                                            href={`/view/${LeagueGames.indexOf(
-                                                game
-                                            )}`}
-                                        >
-                                            <a>
-                                                <div
-                                                    className={
-                                                        styles.HighlightDiv
-                                                    }
+export default function League({ Games }: GamesProps) {
+    const router = useRouter();
+    if (Games) {
+        const LeagueGames = Games.filter(
+            (game) => game.competition.id.toString() === router.query.id
+        );
+        return (
+            <>
+                <Head>
+                    <title>
+                        {LeagueGames[0].competition.name.split(":")[1]}
+                    </title>
+                </Head>
+                <div style={{ width: "90%", margin: "auto" }}>
+                    <Header as="h2" style={{ paddingTop: "1.875rem" }}>
+                        {LeagueGames[0].competition.name.split(":")[1]}
+                    </Header>
+                    <Divider />
+                    <Grid columns={3}>
+                        <Grid.Row>
+                            {LeagueGames.map((game) => (
+                                <Grid.Column>
+                                    <Link href={`/view/${Games.indexOf(game)}`}>
+                                        <a>
+                                            <div
+                                                className={styles.HighlightDiv}
+                                            >
+                                                <Image src={game.thumbnail} />
+                                                <strong
+                                                    style={{
+                                                        fontSize: "1.125rem",
+                                                    }}
                                                 >
-                                                    <Image
-                                                        src={game.thumbnail}
-                                                    />
-                                                    <strong
-                                                        style={{
-                                                            fontSize:
-                                                                "1.125rem",
-                                                        }}
-                                                    >
-                                                        {game.title}
-                                                    </strong>
-                                                    <div>
-                                                        {
-                                                            game.date.split(
-                                                                "T"
-                                                            )[0]
-                                                        }
-                                                    </div>
+                                                    {game.title}
+                                                </strong>
+                                                <div>
+                                                    {game.date.split("T")[0]}
                                                 </div>
-                                            </a>
-                                        </Link>
-                                    </Grid.Column>
-                                ))}
-                            </Grid.Row>
-                        </Grid>
-                    </div>
-                </>
-            )}
-        </>
-    );
+                                            </div>
+                                        </a>
+                                    </Link>
+                                </Grid.Column>
+                            ))}
+                        </Grid.Row>
+                    </Grid>
+                </div>
+            </>
+        );
+    }
+    return <></>;
 }
 
-export const getStaticProps: GetStaticProps = async (context) => {
-    const LeagueID = context?.params?.id;
+export const getStaticProps: GetStaticProps = async () => {
     const res = await axios.get("https://www.scorebat.com/video-api/v1");
-    const data: MatchProps[] = await res.data.filter(
-        (game: MatchProps) => game.competition.id.toString() == LeagueID
-    );
-
+    const data: MatchProps[] = await res.data;
     return {
         props: {
-            LeagueGames: data,
+            Games: data,
         },
         revalidate: 300,
     };
