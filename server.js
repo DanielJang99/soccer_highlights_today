98002c442cc34a119a2dcb306602da1d6c51b398
DanielJang <daniel.jang.0621@gmail.com>
2021-04-07 02:00:05 +0900

    fixed for deployment

--- server.js
+++ server.js
@@ -1,46 +1,46 @@
-const express = require("express");
-const next = require("next");
-const { createProxyMiddleware } = require("http-proxy-middleware");
+// const express = require("express");
+// const next = require("next");
+// const { createProxyMiddleware } = require("http-proxy-middleware");
 
-const port = process.env.PORT;
-const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev });
-const handle = app.getRequestHandler();
+// const port = process.env.PORT;
+// const dev = process.env.NODE_ENV !== "production";
+// const app = next({ dev });
+// const handle = app.getRequestHandler();
 
-const apiPaths = {
-    "/users": {
-        target: process.env.HOST,
-        pathRewrite: {
-            "^/users": "/users",
-        },
-        changeOrigin: true,
-    },
-};
+// const apiPaths = {
+//     "/users": {
+//         target: process.env.HOST,
+//         pathRewrite: {
+//             "^/users": "/users",
+//         },
+//         changeOrigin: true,
+//     },
+// };
 
-const isDevelopment = process.env.NODE_ENV !== "production";
+// const isDevelopment = process.env.NODE_ENV !== "production";
 
-app.prepare()
-    .then(() => {
-        const server = express();
-        server.use(express.json());
-        server.use(express.urlencoded());
-        server.use("/users", createProxyMiddleware(apiPaths["/users"]));
+// app.prepare()
+//     .then(() => {
+//         const server = express();
+//         server.use(express.json());
+//         server.use(express.urlencoded());
+//         server.use("/users", createProxyMiddleware(apiPaths["/users"]));
 
-        server.all("*", (req, res) => {
-            return handle(req, res);
-        });
+//         server.all("*", (req, res) => {
+//             return handle(req, res);
+//         });
 
-        server.listen(port, (err) => {
-            console.log(process.env.HOST);
-            if (err) throw err;
-            if (isDevelopment) {
-                console.log(`> Ready on http://localhost:${port}`);
-            } else {
-                console.log("Ready on production server");
-            }
-        });
-    })
-    .catch((err) => {
-        console.log("Error:::::", err);
-        process.exit(1);
-    });
+//         server.listen(port, (err) => {
+//             console.log(process.env.HOST);
+//             if (err) throw err;
+//             if (isDevelopment) {
+//                 console.log(`> Ready on http://localhost:${port}`);
+//             } else {
+//                 console.log("Ready on production server");
+//             }
+//         });
+//     })
+//     .catch((err) => {
+//         console.log("Error:::::", err);
+//         process.exit(1);
+//     });
