06894e14d267f237abaea37e9daf4153e5f5f5b1
DanielJang <daniel.jang.0621@gmail.com>
2021-04-07 12:00:09 +0900

    fixed typo

--- pages/signup.js
+++ pages/signup.js
@@ -28,7 +28,7 @@ export default function SignUp() {
             setWarningMsg("You must re-enter the same password");
             return;
         }
-        const url = process.env.NEXT_PUBLIC_API_URL + "/user";
+        const url = process.env.NEXT_PUBLIC_API_URL + "/users";
         axios
             .post(url, {
                 username,
